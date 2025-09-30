e7394f0c4d0f5f45b48375eac19b22667fadf956
mgckaled <marcelgckaled@gmail.com>
2025-09-30 14:09:05 -0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rs_full-stack_n3_cupcake-recipe/"
+  base: '/rs_full-stack_n3_cupcake-recipe/'
 })
